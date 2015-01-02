c210a30fa51c4253e8e2a9cdbd15c4266ad7080b
Ethan Jewett <esjewett@gmail.com>
2015-01-02 14:35:19 +0100

    Updates for DropBox compatibility

--- src/components/palladio-map-view/palladio-map-view.js
+++ src/components/palladio-map-view/palladio-map-view.js
@@ -668,6 +668,9 @@ angular.module('palladioMapView', ['palladio', 'palladio.services'])
 			       		[ 1, d3.max(nodes.features, function(d){ return d.properties.value.data.agg; }) ]).range([3,26]);
 		        }
 
+		        // Set up HTTPS URLS for 1.0 API
+		        L.mapbox.config.HTTP_URLS = ["https://a.tiles.mapbox.com/v3/", "https://b.tiles.mapbox.com/v3/", "https://c.tiles.mapbox.com/v3/", "https://d.tiles.mapbox.com/v3/"]
+
 				// init map
 				var node,
 					link,
