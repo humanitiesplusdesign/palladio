298bb474d226b399680b4c90149fcfeb59cd1c6f
Ethan Jewett <esjewett@gmail.com>
2015-01-27 16:04:19 -0600

    Fix issue where file label change doesn't cause data reprocessing

--- src/js/directives/files.js
+++ src/js/directives/files.js
@@ -59,6 +59,8 @@ angular.module('palladio.directives.files', [
 					dataService.setDirty();
 				};
 
+				scope.setDirty = dataService.setDirty;
+
 				scope.downloadFile = function(file) {
 					var blob = new Blob(
 						[ d3.csv.format(file.data) ],
