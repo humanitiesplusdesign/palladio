c6d145e15c76f8fbcd1c0a4222c7bfef98470d90
Ethan Jewett <esjewett@gmail.com>
2015-02-12 16:47:00 -0600

    Fix date handling for ISO-ish dates

--- src/js/services/validation.js
+++ src/js/services/validation.js
@@ -1,9 +1,16 @@
-angular.module('palladio.services.validation', [])
+angular.module('palladio.services.validation', ['palladio.services.date'])
 
-	.factory("validationService", function() {
+	.factory("validationService", ['dateService', function(dateService) {
 		return function (uniques, type) {
 			var errors = [];
-			var dateTest = RegExp('^([-]?\\d{1,4}|[-]?\\d{1,4}[-](0[1-9]|1[012]|[1-9])[-](0[1-9]|[12][0-9]|3[01]|[1-9]))$');
+			var dateTest = RegExp('^(([-]?\\d{1,4})|' +   // YYYY, -YYYY
+				'([-]?\\d{1,4}[-](0[1-9]|1[012]|[1-9])[-](0[1-9]|[12][0-9]|3[01]|[1-9]))|' + // [-]YYYY-MM-DD
+				// ISO formats
+				'([-]?\\d{1,4}[-](0[1-9]|1[012]|[1-9])[-](0[1-9]|[12][0-9]|3[01]|[1-9])T[0-2]\\d:[0-5]\\d:[0-5]\\d\\.\\d+([+-][0-2]\\d:[0-5]\\d|[Z]?))|' +
+				'([-]?\\d{1,4}[-](0[1-9]|1[012]|[1-9])[-](0[1-9]|[12][0-9]|3[01]|[1-9])T[0-2]\\d:[0-5]\\d:[0-5]\\d([+-][0-2]\\d:[0-5]\\d|[Z]?))|' +
+				'([-]?\\d{1,4}[-](0[1-9]|1[012]|[1-9])[-](0[1-9]|[12][0-9]|3[01]|[1-9])T[0-2]\\d:[0-5]\\d([+-][0-2]\\d:[0-5]\\d|[Z]?))' +
+				')$');
+			// ||
 			var latlongTest = RegExp('^(\\-?\\d+(\\.\\d+)?),\\s*(\\-?\\d+(\\.\\d+)?)$');
 
 			uniques.forEach(function (d) {
@@ -12,7 +19,13 @@ angular.module('palladio.services.validation', [])
 						if(isNaN(+d)) errors.push({ value: d, message: '"' + d + '" is not a number' });
 						break;
 					case 'date':
-						if(!dateTest.test(d)) errors.push({ value: d, message: '"' + d + '" is not a date' });
+						if(d && !dateService.format.parseExternal(d)) {
+							errors.push({ value: d, message: '"' + d + '" is not a date' });
+							break;
+						}
+						if(!dateTest.test(d)) {
+							errors.push({ value: d, message: '"' + d + '" has our of range values'});
+						}
 						break;
 					case 'latlong':
 						if(!latlongTest.test(d)) errors.push({ value: d, message: '"' + d + '" is not a lat/long pair'});
@@ -24,4 +37,4 @@ angular.module('palladio.services.validation', [])
 
 			return errors;
 		};
-	});
+	}]);
