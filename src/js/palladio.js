08be102995b13230f221456ed9eb58f5dd38cdc9
Ethan Jewett <esjewett@gmail.com>
2015-07-13 12:42:29 -0500

    Increment versions

--- src/js/palladio.js
+++ src/js/palladio.js
@@ -5,7 +5,7 @@
 // for usage examples.
 
 angular.module('palladio', [])
-	.constant('version', '1.0.0')
+	.constant('version', '1.0.1')
 	.factory('palladioService', ['$compile', "$rootScope", function($compile, $scope) {
 
 		var updateListeners = d3.map();
