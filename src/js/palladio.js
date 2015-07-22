26d98c07e5a84115afb5672c13ee81c134acd8be
Ethan Jewett <esjewett@gmail.com>
2015-07-22 12:15:20 -0500

    Update versions

--- src/js/palladio.js
+++ src/js/palladio.js
@@ -5,7 +5,7 @@
 // for usage examples.
 
 angular.module('palladio', [])
-	.constant('version', '1.0.1')
+	.constant('version', '1.0.2')
 	.factory('palladioService', ['$compile', "$rootScope", function($compile, $scope) {
 
 		var updateListeners = d3.map();
