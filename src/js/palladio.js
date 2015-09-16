85914395e8a4e98ce586ce542ff940fe11e60e37
Ethan Jewett <esjewett@gmail.com>
2015-09-16 13:47:48 -0500

    Move to alpha.02 version in preparation for new alpha release

--- src/js/palladio.js
+++ src/js/palladio.js
@@ -5,7 +5,7 @@
 // for usage examples.
 
 angular.module('palladio', [])
-	.constant('version', '1.1.0-alpha.01')
+	.constant('version', '1.1.0-alpha.02')
 	.factory('palladioService', ['$compile', "$rootScope", function($compile, $scope) {
 
 		var updateListeners = d3.map();
