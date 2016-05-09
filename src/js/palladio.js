228f0e3de66de2ccd82c735dc624f84de3531e4b
Ethan Jewett <esjewett@gmail.com>
2016-05-09 18:13:14 -0500

    Increment version number for next beta release

--- src/js/palladio.js
+++ src/js/palladio.js
@@ -5,7 +5,7 @@
 // for usage examples.
 
 angular.module('palladio', [])
-	.constant('version', '1.2.0-beta.06')
+	.constant('version', '1.2.0-beta.08')
 	.factory('palladioService', ['$compile', "$rootScope", '$q', function($compile, $scope, $q) {
 
 		var updateListeners = d3.map();
