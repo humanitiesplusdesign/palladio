6f2a551f259d9efe33577a14b931bdc4614cdfe0
Ethan Jewett <esjewett@gmail.com>
2016-05-18 11:52:34 -0500

    Update gulp-uglify

--- palladio.js
+++ palladio.js
@@ -110640,57 +110640,6 @@ angular.module('palladioDataDownload', ['palladio.services', 'palladio'])
 
 		return directiveObj;
 	}]);
-// Palladio data upload component
-
-angular.module('palladioDataUpload', ['palladio.services'])
-	.run(['componentService', function(componentService) {
-		var compileStringFunction = function (options, newScope) {
-
-			newScope.onLoad = options.loadFunction;
-			var compileString = '<button class="btn" palladio-data-upload on-load="onLoad()">';
-			compileString += '<i class="fa fa-upload"></i>&nbsp;';
-			compileString += 'Import a Palladio data model</button>';
-
-			return compileString;
-		};
-
-		componentService.register('upload', compileStringFunction);
-	}])
-	.directive('palladioDataUpload', ['dataService', 'loadService', 'spinnerService', function (dataService, loadService, spinnerService) {
-		var directiveObj = {
-			scope: {
-				'load': '&onLoad',
-			},
-			transclude: true,
-			templateUrl: 'partials/palladio-data-upload/template.html',
-
-			link: function(scope) {
-
-				scope.loadDataModel = function(input) {
-					spinnerService.spin();
-					var reader = new FileReader();
-					reader.onload = function() {
-						var json = JSON.parse(reader.result);
-						loadService.loadJson(json).then(function() {
-							scope.$apply(function(s) {
-								s.load(json);
-							});
-						});
-					};
-					reader.readAsText(input.files[0]);
-					// We need to clear the input so that we pick up future uploads. This is *not*
-					// cross-browser-compatible.
-					input.value = null;
-				};
-
-				scope.triggerDataModelSelector = function () {
-					$('#dataModelSelector').click();
-				};
-			}
-		};
-
-		return directiveObj;
-	}]);
 // Selection view module
 
 angular.module('palladioSelectionView', ['palladio'])
@@ -110783,6 +110732,57 @@ angular.module('palladioSelectionView', ['palladio'])
 		return directiveDefObj;
 	}]);
 
+// Palladio data upload component
+
+angular.module('palladioDataUpload', ['palladio.services'])
+	.run(['componentService', function(componentService) {
+		var compileStringFunction = function (options, newScope) {
+
+			newScope.onLoad = options.loadFunction;
+			var compileString = '<button class="btn" palladio-data-upload on-load="onLoad()">';
+			compileString += '<i class="fa fa-upload"></i>&nbsp;';
+			compileString += 'Import a Palladio data model</button>';
+
+			return compileString;
+		};
+
+		componentService.register('upload', compileStringFunction);
+	}])
+	.directive('palladioDataUpload', ['dataService', 'loadService', 'spinnerService', function (dataService, loadService, spinnerService) {
+		var directiveObj = {
+			scope: {
+				'load': '&onLoad',
+			},
+			transclude: true,
+			templateUrl: 'partials/palladio-data-upload/template.html',
+
+			link: function(scope) {
+
+				scope.loadDataModel = function(input) {
+					spinnerService.spin();
+					var reader = new FileReader();
+					reader.onload = function() {
+						var json = JSON.parse(reader.result);
+						loadService.loadJson(json).then(function() {
+							scope.$apply(function(s) {
+								s.load(json);
+							});
+						});
+					};
+					reader.readAsText(input.files[0]);
+					// We need to clear the input so that we pick up future uploads. This is *not*
+					// cross-browser-compatible.
+					input.value = null;
+				};
+
+				scope.triggerDataModelSelector = function () {
+					$('#dataModelSelector').click();
+				};
+			}
+		};
+
+		return directiveObj;
+	}]);
 // Palladio template component module
 
 angular.module('palladioIdiographView', ['palladio', 'palladio.services'])
