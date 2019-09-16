48b94e404687ca4d092b9f1271cef5d3ea208177
Simon Wiles <simonjwiles@gmail.com>
2019-09-16 14:55:31 -0700

    Minify CSS bundle
    Not sure this is wise -- if these assets are only every used as part of a larger project, they would probably be better bundled and optimized at that level.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 const webpack = require("webpack");
+
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const isDev = process.env.NODE_ENV !== "production";
 
 module.exports = {
   mode: "development",
   entry: "./src/entrypoint.js",
   output: {
-    filename: "palladio.js",
+    filename: isDev ? "palladio.js" : "palladio.min.js",
     path: path.resolve(__dirname, "dist/")
   },
   module: {
@@ -33,6 +38,9 @@ module.exports = {
       }
     ]
   },
+  optimization: {
+    minimizer: isDev ? [] : [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
   plugins: [
     new webpack.ProvidePlugin({
       d3: "d3"
@@ -41,6 +49,6 @@ module.exports = {
       // "window.jQuery": "jquery",
       // "window.$": "jquery"
     }),
-    new MiniCssExtractPlugin({ filename: "palladio.css" })
+    new MiniCssExtractPlugin({ filename: isDev ? "palladio.css": "palladio.min.css" })
   ]
 };
